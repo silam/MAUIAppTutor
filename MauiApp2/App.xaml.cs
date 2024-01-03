3792769b8625aed0584d558efc491e091e26e7b6
Si Lam <silam@hotmail.com>
2024-01-03 16:27:00 -0600

    First commit Navigation Page

---
+++ MauiApp2/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiApp2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var navPage = new NavigationPage(new MainPage());
+            navPage.BarBackgroundColor = Colors.Chocolate;
+            navPage.BarTextColor = Colors.White;
+            MainPage = navPage;
+            //new AppShell();
+            //MainPage = new ContentPageDemo();
+        }
+    }
+}
---
+++ MauiApp2/ContentPageDemo.xaml.cs
@@ -0,0 +1,14 @@
+namespace MauiApp2;
+
+public partial class ContentPageDemo : ContentPage
+{
+	public ContentPageDemo()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		Navigation.PushAsync(new MainPage());
+    }
+}
