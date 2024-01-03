849972f5ec27c7a66b932d74673426b5da9bdcce
Si Lam <silam@hotmail.com>
2024-01-03 16:52:11 -0600

    Flyout page

--- MauiApp2/App.xaml.cs
+++ MauiApp2/App.xaml.cs
@@ -6,12 +6,12 @@
         {
             InitializeComponent();
 
-            var navPage = new NavigationPage(new MainPage());
-            navPage.BarBackgroundColor = Colors.Chocolate;
-            navPage.BarTextColor = Colors.White;
-            MainPage = navPage;
+            //var navPage = new NavigationPage(new MainPage());
+            //navPage.BarBackgroundColor = Colors.Chocolate;
+            //navPage.BarTextColor = Colors.White;
+            //MainPage = navPage;
             //new AppShell();
-            //MainPage = new ContentPageDemo();
+            MainPage = new FlyoutPageDemo();
         }
     }
 }
---
+++ MauiApp2/FlyoutPageDemo.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp2;
+
+public partial class FlyoutPageDemo : FlyoutPage
+{
+	public FlyoutPageDemo()
+	{
+		InitializeComponent();
+	}
+}
