35ae086741789da1d6e4fa8cf8ffb95322f241af
Si Lam <silam@hotmail.com>
2024-01-17 10:25:49 -0600

    CollectionView

--- MauiApp2/App.xaml.cs
+++ MauiApp2/App.xaml.cs
@@ -12,7 +12,7 @@
             //MainPage = navPage;
             //new AppShell();
             //MainPage = new FlexLayoutDemo();
-            MainPage = new ActivityControlDemo();
+            MainPage = new CollectionsControlDemo();
         }
     }
 }
---
+++ MauiApp2/CollectionsControlDemo.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp2;
+
+public partial class CollectionsControlDemo : ContentPage
+{
+	public CollectionsControlDemo()
+	{
+		InitializeComponent();
+	}
+}
